ea601a3b484ebc0ac5de8137111f39a8cc703ee2
smith494 <court14104@gmail.com>
2026-07-16 07:11:22 -0500

    completed unittest training for python

---
+++ python_bootcamp_files/cap.py
@@ -0,0 +1,5 @@
+def cap_text(text):
+    '''
+    Capitalize the text of a string
+    '''
+    return text.title()
---
+++ python_bootcamp_files/simpleone.py
@@ -0,0 +1,14 @@
+'''
+A very simple python program that adds two numbers and prints the result.
+'''
+def myfunc(): 
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
+
